8c3d918e60f23f6121404363604f0cdd34532c33
Albert Zhou <albertwzhou2@gmail.com>
2018-08-29 08:49:47 -0700

    Added countdowntimer to switch to turn off after 1 second. Switching off early cancels the timer.

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.uselessmachine;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -11,6 +13,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked) {
+                    startSwitchOffTimer();
                     Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
                 }
                 else {
@@ -38,6 +43,23 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void startSwitchOffTimer() {
+        new CountDownTimer(1000, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if(!switchUseless.isChecked()) {
+                    //Log.d(TAG, "onTick: cancelling");
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                switchUseless.setChecked(false);
+                //Log.d(TAG, "onFinish: switch set to false");
+            }
+        }.start();
     }
 
     /**
