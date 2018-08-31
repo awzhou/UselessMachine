8cafc562bcdd29a5ffd66dc5647262f23df989fb
Albert Zhou <albertwzhou2@gmail.com>
2018-08-31 08:14:18 -0700

    Finished project

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.uselessmachine;
 
+import android.graphics.Color;
 import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -14,7 +18,12 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
 
-    public static final String TAG = MainActivity.class.getSimpleName();
+    private ConstraintLayout constraintLayout;
+
+    private int selfDestructCounter;
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +39,64 @@ public class MainActivity extends AppCompatActivity {
      */
     private void setListeners() {
         // TODO self destruct button
+        buttonSelfDestruct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSelfDestructSequence();
+            }
+        });
 
         switchUseless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked) {
                     startSwitchOffTimer();
-                    Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
                 }
-                else {
-                    Toast.makeText(MainActivity.this, "Off", Toast.LENGTH_SHORT).show();
-                }
+
             }
         });
+    }
+
+    private void startSelfDestructSequence() {
+        // Disable the button
+        buttonSelfDestruct.setEnabled(false);
+
+        // Start a 10 second countdown timer that updates the display every second
+        startSelfDestructTimer();
+
+        // Want the button to show the countdown
+        // Destruct in 10...
+        // Destruct in 9...
+
+        // At the end, we're going to close the activity
+        // call the finish() method
+    }
+
+    private void startSelfDestructTimer() {
+        selfDestructCounter = 1;
+        new CountDownTimer(10000, 500) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                //Log.d(TAG, "onTick: Ticked");
+                if(selfDestructCounter % 2 == 0) {
+                    buttonSelfDestruct.setText("Destruct in " + (millisUntilFinished + 1000) / 1000 + "...");
+                    constraintLayout.setBackgroundColor(Color.rgb(255, 0, 0));
+                }
+                else {
+                    constraintLayout.setBackgroundColor(Color.rgb(255, 255, 255));
+                }
+
+                selfDestructCounter++;
+
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+            }
+        }.start();
     }
 
     private void startSwitchOffTimer() {
@@ -69,5 +123,6 @@ public class MainActivity extends AppCompatActivity {
     private void wireWidgets() {
         buttonSelfDestruct = findViewById(R.id.button_main_selfdestruct);
         switchUseless = findViewById(R.id.switch_main_useless);
+        constraintLayout = findViewById(R.id.activity_main);
     }
 }
